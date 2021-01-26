7efc1b8919f98f03d79edc3e6cb3fb6bf9b82ca0
nicholas-hibbits <nicholashibbits@gmail.com>
2021-01-26 13:16:01 -0800

    Formatted JavaScript code. Last check before submission.

--- server.js
+++ server.js
@@ -2,48 +2,44 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
 const port = 8000;
 
-const server = app.listen(port, ()=>{
-  console.log(`yea bb running on localhost: ${port}`)})
+const server = app.listen(port, () => {
+    console.log(`Server is running on localhost: ${port}`);
+});
 
 // POST route
-app.post('/add', function(req, res){
-  // console.log(req.body);
-  let data = req.body;
-  newEntry = {
-     temperature: data.temperature,
-     date: data.date,
-     entry: data.entry
-  }
-  projectData['entry'] = newEntry;
-  // console.log(projectData)
-  res.send(projectData)
-  // console.log(projectData);
+app.post("/add", function (req, res) {
+    let data = req.body;
+    newEntry = {
+        temperature: data.temperature,
+        date: data.date,
+        entry: data.entry,
+    };
+    projectData["entry"] = newEntry;
 });
 
 // GET route
-app.get('/updatePage', function(req, res){
-  res.send(projectData);
-  console.log(projectData)
+app.get("/updatePage", function (req, res) {
+    res.send(projectData);
+    console.log(projectData);
 });
--- website/app.js
+++ website/app.js
@@ -1,71 +1,65 @@
 /* Global Variables */
-// let baseURL = 'api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}';
-let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-let apiKey = '&appid=cf671a0e6004b23e0fa755be924ec763';
+let baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+let apiKey = "&appid=cf671a0e6004b23e0fa755be924ec763";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
 // Do the following when the generate button is clicked
-document.getElementById('generate').addEventListener('click', performAction);
+document.getElementById("generate").addEventListener("click", performAction);
 
-// GET weather info from API then POST that info, along with user's entry, to the site
-function performAction(e){
-  let newZip = document.getElementById('zip').value;
-  let feelings = document.getElementById('feelings').value;
-  getWeather(baseURL, newZip, apiKey)
-  .then(function(data){
-    console.log(data)
-    postData('/add', {temperature:data.main.temp, date:newDate, entry:feelings});
-    update();
-  })
+// GET weather info from API, then POST that info, along with user's entry, to the UI
+function performAction(e) {
+    let newZip = document.getElementById("zip").value;
+    let feelings = document.getElementById("feelings").value;
+    getWeather(baseURL, newZip, apiKey).then(function (data) {
+        postData("/add", { temperature: data.main.temp, date: newDate, entry: feelings });
+        update();
+    });
 }
 
-// Update UI
-const update = async()=>{
-  const request = await fetch('/updatePage');
-  try{
-  const allData = await request.json();
-  console.log(allData);
-  document.getElementById('temp').innerHTML = allData['entry'].temperature;
-  document.getElementById('date').innerHTML = allData['entry'].date;
-  document.getElementById('content').innerHTML = allData['entry'].entry;
-  }
-  catch(error){
-  console.log("error", error);
-  }
-}
 // Access the OpenWeather API to get local weather data based on the zip code
-const getWeather = async (baseURL, zip, key)=>{
-  const res = await fetch(baseURL+zip+key)
-  try {
-    const data = await res.json();
-    console.log(data)
-    return data;
-  }
-  catch(error) {
-    console.log("error", error);
-  }
-}
+const getWeather = async (baseURL, zip, key) => {
+    const res = await fetch(baseURL + zip + key);
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log("error", error);
+    }
+};
 
 // POST request to grab data returned from OpenWeather API
-const postData = async (url = '', newInfo = {})=>{
+const postData = async (url = "", newInfo = {}) => {
     const response = await fetch(url, {
-      method: 'POST',
-      credentials: 'same-origin',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-     // Body data type must match "Content-Type" header
-      body: JSON.stringify(newInfo),
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        // Body data type must match "Content-Type" header
+        body: JSON.stringify(newInfo),
     });
-      try {
+    try {
         const newData = await response.json();
-        console.log(newData);
         return newData;
-      }
-      catch(error) {
-      console.log("error", error);
-      }
-}
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+// Update UI
+const update = async () => {
+    const request = await fetch("/updatePage");
+    try {
+        const allData = await request.json();
+        // console.log(allData);
+        document.getElementById("temp").innerHTML = allData["entry"].temperature;
+        document.getElementById("date").innerHTML = allData["entry"].date;
+        document.getElementById("content").innerHTML = allData["entry"].entry;
+    } catch (error) {
+        console.log("error", error);
+    }
+};
